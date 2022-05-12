ebe83457b297e74ad72d3abc53c9d58975344feb
Klayking0 <105227096+Klayking0@users.noreply.github.com>
2022-05-12 21:27:06 +0100

    Date!

--- js/draw.js
+++ js/draw.js
@@ -4,14 +4,17 @@ setInterval(() => {
 	min = d.getMinutes();
 	sec = d.getSeconds();
 	ms = d.getMilliseconds();
+	dat = d.getDate();
 	hr_rotation = 30 * hr; //converting current time
 	min_rotation = 6 * min;
 	sec_rotation = 6 * sec;
 	ms_rotation = ms / 166.666667;
+	dat_rotation = 11.61290322580645 * (dat-1);
 
 	hour.style.transform = `rotate(${(hr_rotation)+(min_rotation/12)+(sec_rotation/720)}deg)`;
 	minute.style.transform = `rotate(${(min_rotation)+(sec_rotation/60)+(ms_rotation/166.666667)}deg)`;
 	second.style.transform = `rotate(${(sec_rotation)+(ms_rotation)}deg)`;
+	date.style.transform = `rotate(${(dat_rotation)}deg)`;
 	
 }, 166.66667);
 
