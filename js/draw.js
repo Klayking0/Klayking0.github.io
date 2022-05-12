aea1f09c6c96c24ae60af6db1a7c5bad40488ebb
Klayking0 <105227096+Klayking0@users.noreply.github.com>
2022-05-12 21:40:00 +0100

    Experimental full screen

--- js/draw.js
+++ js/draw.js
@@ -18,7 +18,36 @@ setInterval(() => {
 	
 }, 166.66667);
 
+//Day-night mode toggle
+	const nightbtn = document.getElementById('cocktail1');
+	nightbtn.addEventListener('click', function onClick(event) {
+		openFullscreen()
+	});
 
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
 
 //const watchContainer = document.getElementById('watchContainer');
 //watchContainer.innerHTML = str;
