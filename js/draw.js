ae763949cae3e9ee87b83bbef64336bc423c6ba9
Klayking0 <105227096+Klayking0@users.noreply.github.com>
2022-05-14 13:49:13 +0100

    More attempted mobile fix

--- js/draw.js
+++ js/draw.js
@@ -76,13 +76,6 @@ window.onload = function() {
 };
 */
 
-window.addEventListener('resize', () => {
-	// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-	let vh = window.innerHeight * 0.01;
-	// Then we set the value in the --vh custom property to the root of the document
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 //Select an element to be full screen button
 const fsButton = document.getElementById('fullscreenButton');
 fsButton.addEventListener('click', function onClick(event) {
